34d49011f186ee85ea6c61bcfa15c9b67f50b71c
M4cXD <jcarlos.rojas.lol@gmail.com>
2024-06-23 14:40:19 -0600

    Add files via upload
    logica-programacion-DESAFIO1-JCRL

---
+++ logica-programacion-DESAFIO1-JCRL/app.js
@@ -0,0 +1,55 @@
+//Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+alert("Bienvenida y bienvenido a nuestro sitio web!");
+
+//Variables
+//Declara una variable llamada nombre y asígnale el valor "Luna".
+let nombre="Luna";
+console.log(nombre);
+
+//Crea una variable llamada edady asígnale el valor 25.
+let edady=25;
+console.log(edady);
+
+//Establece una variable numeroDeVentas y asígnale el valor 50.
+let numeroDeVentas=50;
+console.log(numeroDeVentas);
+
+//Establece una variable saldoDisponible y asígnale el valor 1000.
+let saldoDisponible=1000;
+console.log(saldoDisponible);
+
+//Muestra una alerta con el texto "¡Error! Completa todos los campos".
+alert("¡Error! Completa todos los campos");
+
+//Declara una variable llamada mensajeDeError y asígnale el valor "Completa todos los campos".
+//Variable
+let mensajeDeError="Completa todos los campos";
+console.log(mensajeDeError);
+
+//Ahora muestra una alerta con el valor de la variable mensajeDeError .
+alert(mensajeDeError);
+
+//Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombreUser .
+//Variable
+let nombreUser = prompt("Cual es tu nombre?");
+console.log(nombreUser);
+
+//Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+//Variable
+let edad = prompt("Cual es tu edad?");
+console.log(edad);
+
+//Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+/*
+Este código realiza
+la comparación
+*/
+if (edad >= 18) {
+     //Fue verdadera la condición
+    alert("¡Puedes obtener tu licencia de conducir!");
+} else {
+    //La condición no se cumplió
+    alert('Lo siento, aun no cumples con la EDAD necesaria');
+}
+
+//ITS DONE!!...=)
